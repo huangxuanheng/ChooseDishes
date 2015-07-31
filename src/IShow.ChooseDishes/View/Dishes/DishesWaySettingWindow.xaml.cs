a954a50da0a5c0fc8240674e796db597e799dd42
huangxuanheng <huangxuanheng@163.com>
2015-07-31 19:54:28 +0800

    换了框架，使用databse first框架，丢弃联想搭好的框架

---
+++ src/IShow.ChooseDishes/View/Dishes/DishesWaySettingWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IShow.ChooseDishes.View.DischesWayView
+{
+    /// <summary>
+    /// Interaction logic for DishesWayFrom.xaml
+    /// </summary>
+    public partial class DishesWaySettingWindow : Window
+    {
+        public DishesWaySettingWindow()
+        {
+            //MessageBox.Show("弹出做法页面");
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
